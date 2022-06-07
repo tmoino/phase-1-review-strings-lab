479adfc5e822b7463403bfcf9cb610d74679256e
tmoino <tony.moino@student.moringaschool.com>
2022-06-07 13:15:08 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser ="Grace Hopper"
+const welcomeMessage = "Welcome to Flatbook, "+ currentUser + "!"
+let excitedWelcomeMessage =  welcomeMessage.toUpperCase() + currentUser.toUpperCase() + "!" 
+var shortGreeting = "Welcome, " + (currentUser.slice(0,1)) + "!"
